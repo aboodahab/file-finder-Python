c04ce46b2fe8287f64901083add24374e983215d
Meow <abodkaramma77@gmail.com>
2025-01-27 22:36:37 +0200

    S

--- engine.py
+++ engine.py
@@ -4,16 +4,16 @@ import os
 import argparse
 def data():
  parser = argparse.ArgumentParser(description="A script that demonstrates help functionality.")
- parser.add_argument("--type", type=str, help="Specify your name.",default="txt")
- parser.add_argument("--place", type=str, help="Specify your age.",default=".")
+ parser.add_argument("--type", type=str, help="Specify your files typpe.")
+ parser.add_argument("--place", type=str, help="Specify your files place .",default=".")
  args = parser.parse_args()
  place=args.place
- type=args.type
+ type=args.type 
  if place==".":
     place=os.getcwd()
  result=[]
  return type,place,result
-
+ 
 def checkFolders(directory):
  folders = [item for item in os.listdir(directory) if os.path.isdir(os.path.join(directory, item))] 
  if len(folders)>0:
@@ -26,12 +26,19 @@ def checkFolders(directory):
   return
  
 def getFiles(place,type,result):
-  path = f'{place}/*.{type}'
-  files = glob.glob(path)
-  for i in files:
-   result.append(i)
-  print(result)
+  if type==None or type=="": 
+    path = f'{place}'
+    files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+
+    for i in files:
+     print([i],"st") 
+  else: 
+   path = f'{place}/*.{type}'
+   files = glob.glob(path)
+  #  print(os.listdir(f"{place}"),"os")
+   for i in files:
+    result.append(i)
+    print(result,"se")
 
 getFiles(data()[1],data()[0],data()[2]) 
 checkFolders(data()[1])
-
